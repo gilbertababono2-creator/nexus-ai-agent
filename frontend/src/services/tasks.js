e4a9bee82b30038c59cce2bd7959e4abbbf0a190
gilbertababono2-creator <gilbertababono2@gmail.com>
2026-05-21 09:27:52 +0000

    Add task fetching and completion functions

--- frontend/src/services/tasks.js
+++ frontend/src/services/tasks.js
@@ -1,1 +1,12 @@
 
+import api from './api';
+
+export const fetchTasks = async (userId) => {
+  const res = await api.get(`/tasks/${userId}`);
+  return res.data;
+};
+
+export const completeTask = async (taskId) => {
+  const res = await api.post(`/tasks/complete/${taskId}`);
+  return res.data;
+};
