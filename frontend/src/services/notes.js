7c127dedec3f8abb39a34a73511300b8339c23b4
gilbertababono2-creator <gilbertababono2@gmail.com>
2026-05-21 09:31:05 +0000

    Add fetchNotes and createNote functions

--- frontend/src/services/notes.js
+++ frontend/src/services/notes.js
@@ -1,1 +1,11 @@
+import api from './api';
 
+export const fetchNotes = async (userId) => {
+  const res = await api.get(`/notes/${userId}`);
+  return res.data;
+};
+
+export const createNote = async (userId, noteData) => {
+  const res = await api.post('/notes', { userId, ...noteData });
+  return res.data;
+};
