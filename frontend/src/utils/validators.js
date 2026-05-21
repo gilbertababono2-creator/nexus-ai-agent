47149658c80a9aa523d9c2508b0ab26abe583422
gilbertababono2-creator <gilbertababono2@gmail.com>
2026-05-21 09:35:16 +0000

    Add validation functions for email, date, and time

--- frontend/src/utils/validators.js
+++ frontend/src/utils/validators.js
@@ -1,1 +1,13 @@
+export const isValidEmail = (email) => {
+  const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return re.test(email);
+};
 
+export const isValidDate = (date) => {
+  return !isNaN(Date.parse(date));
+};
+
+export const isValidTime = (time) => {
+  const re = /^([01]\d|2[0-3]):([0-5]\d)$/;
+  return re.test(time);
+};
