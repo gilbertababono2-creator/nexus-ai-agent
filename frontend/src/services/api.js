b60e951f25602e1a6eab2faf946bf1b3989c8ea2
gilbertababono2-creator <gilbertababono2@gmail.com>
2026-05-21 09:09:50 +0000

    Set up Axios instance with request interceptor

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,1 +1,15 @@
+import axios from 'axios';
+import { API_BASE_URL } from '../config/constants';
 
+const api = axios.create({
+  baseURL: API_BASE_URL,
+  headers: { 'Content-Type': 'application/json' }
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+export default api;
