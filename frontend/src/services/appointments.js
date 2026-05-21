f3922ead8e29d521e4e3049f19bf4d645bacdcb6
gilbertababono2-creator <gilbertababono2@gmail.com>
2026-05-21 09:30:25 +0000

    Add fetch and delete appointment functions

--- frontend/src/services/appointments.js
+++ frontend/src/services/appointments.js
@@ -1,1 +1,11 @@
+import api from './api';
 
+export const fetchAppointments = async (userId) => {
+  const res = await api.get(`/appointments/${userId}`);
+  return res.data;
+};
+
+export const deleteAppointment = async (appointmentId) => {
+  const res = await api.delete(`/appointments/${appointmentId}`);
+  return res.data;
+};
